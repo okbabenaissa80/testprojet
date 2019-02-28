77714fbc024983bb7a4e8c67745014969b8b98fe
Benaissa <e1795858@cmaisonneuve.qc.ca>
2019-02-28 14:17:00 -0500

    Ajout d'un fichier

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 
 <?php 
-	echo"salut tout le monde"
+	echo"salut tout le monde";
+	echo"salut tout le monde";
+	echo"salut tout le monde";
+	/**
+	* 
+	*/
+	class ClassName extends AnotherClass
+	{
+		
+		function __construct(argument)
+		{
+			# code...
+		}
+	}
 ?>
